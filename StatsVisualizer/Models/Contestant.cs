508ab249027ccabd3dfe6f2ad0df763bd571d95e
caleb.morales <caleb.morales@students.snow.edu>
2020-02-18 21:21:03 -0700

    Add project files.

---
+++ StatsVisualizer/Models/Contestant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatsVisualizer.Models
+{
+    public class Contestant
+    {
+        public string Name { get; set; }
+        public string Token { get; set; }
+
+        public DateTime LastSeen { get; set; }
+
+        public int? GenerationsComputed { get; set; }
+
+        public DateTime? StartedGameAt { get; set; }
+
+        public DateTime? EndedGameAt { get; set; }
+
+        //public TimeSpan? Elapsed { get; set; }
+        public TimeSpan? Elapsed
+        {
+            get
+            {
+                if (StartedGameAt == null || EndedGameAt == null)
+                {
+                    return TimeSpan.MaxValue;
+                }
+                return EndedGameAt - StartedGameAt;
+            }
+        }
+
+        public object FinalBoard { get; set; }
+
+        public bool? CorrectFinalBoard { get; set; }
+
+        
+    }
+    public class Stats
+    {
+        public IEnumerable<Contestant> Contestants { get; set; }
+    }
+}
---
+++ StatsVisualizer/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using StatsVisualizer.Models;
+
+namespace StatsVisualizer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //Replace the serverURL String with the URL of the person running the server. If you are then enter http://localhost.
+        //http://144.17.24.145 is the URL for Jonathans machine
+        private const string serverURL = "http://144.17.48.92/stats";
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Stats Stats { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async void OnGet()
+        {
+            var request = WebRequest.CreateHttp(serverURL);
+            var response = (HttpWebResponse)request.GetResponse();
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                //System.IO.Stream stream = response.GetResponseStream();
+                //var reader = new StreamReader(stream);
+                //var json = reader.ReadToEnd();
+                var options = new JsonSerializerOptions();
+                options.PropertyNameCaseInsensitive = true;
+                //var x = JsonSerializer.Deserialize<Stats>(json, options);
+                Stats = await JsonSerializer.DeserializeAsync<Stats>(response.GetResponseStream(), options);
+                //response.GetResponseStream();
+            }        
+        }
+    }
+}
